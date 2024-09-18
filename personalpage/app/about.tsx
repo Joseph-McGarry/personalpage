c3a773f9f189a2cdc1e2cb41f80bf22e2083040d
Joseph-McGarry <jrmcgarry88@gmail.com>
2024-09-18 13:19:49 -0400

    artist button turned to gray-300. tailwind config file changed to stop purging.

--- personalpage/app/about.tsx
+++ personalpage/app/about.tsx
@@ -13,7 +13,7 @@ export default function About() {
         <br></br>
         <br></br>
         <a 
-          className="cursor-default rounded text-md hover:text-lg hover:bg-white hover:shadow-two hover:text-black transition-shadow duration-1000 ease-in-out"
+          className="cursor-default rounded text-md hover:text-lg hover:bg-gray-300 hover:shadow-two hover:text-black transition-shadow duration-100 ease-in-out"
           href="https://www.instagram.com/vulcanistileco/" 
           target="_blank" 
           rel="noopener noreferrer" 
--- personalpage/tailwind.config.ts
+++ personalpage/tailwind.config.ts
@@ -6,6 +6,22 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  purge: {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    options: {
+      safelist: [
+        'hover:bg-gray-50',
+        'hover:bg-gray-100',
+        'hover:bg-gray-200',
+        'hover:bg-gray-300',
+        'hover:bg-gray-400',
+      ],
+    },
+  },
   theme: {
     extend: {
       backgroundImage: {
@@ -14,11 +30,12 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       boxShadow: {
-        'white': '0 3px 10px 2px rgba(255, 255, 255, 0.1), 0 2px 4px -2px rgba(255, 255, 255, 0.1)  ',
+        'white': '0 3px 10px 2px rgba(255, 255, 255, 0.1), 0 2px 4px -2px rgba(255, 255, 255, 0.1)',
         'two': '0px 0px 70px 30px rgba(255, 255, 255, 1), 0px 0px 50px 120px rgba(225, 225, 225, 0.1)',
       }
     },
   },
   plugins: [],
 };
+
 export default config;
