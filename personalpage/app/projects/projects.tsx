9601c86fce14e14a05360578c6b6d0aa4a732e55
Joseph-McGarry <jrmcgarry88@gmail.com>
2024-10-17 16:46:27 -0400

    abstractify png added with clickability to randomwalk endpoint. removed abstractify button that was like the buttons in links section.

--- personalpage/app/projects/projects.tsx
+++ personalpage/app/projects/projects.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Projects() {
 
@@ -10,9 +11,16 @@ return (
         <div className="grid grid-cols-1 gap-x-1 place-items-center">
             <div  className='m-2'>
                 <Link href="/projects/randomwalk" passHref>
-                    <span className='cursor-default block text-sm w-20 pl-1 pr-1 pt-2 pb-2 rounded text-center hover:shadow-white hover:bg-black transition-shadow duration-300 ease-in-out'>
+                    {/* <span className='cursor-default block text-sm w-20 pl-1 pr-1 pt-2 pb-2 rounded text-center hover:shadow-white hover:bg-black transition-shadow duration-300 ease-in-out'>
                     Abstractify
-                    </span>
+                    </span> */}
+                    <Image
+                        src="/Abstractify.png"
+                        alt="abstractify preview"
+                        width={500}  // specify width
+                        height={300} // specify height
+                        className=" mt-4 w-64 md:w-96 lg:w-128 h-auto"
+                        />
                 </Link>
             </div>
             {/* <div  className='m-2'>
